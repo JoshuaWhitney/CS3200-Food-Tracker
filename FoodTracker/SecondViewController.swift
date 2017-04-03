5c836f152eb3c6ec1f0a9169b72fcb6c76d5a44c
joshua <joshua@Whitney-MacBookPro.local>
2017-04-02 23:02:33 -0600

    Initial Commit

---
+++ FoodTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FoodTracker
+//
+//  Created by Joshua on 4/2/17.
+//  Copyright © 2017 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
