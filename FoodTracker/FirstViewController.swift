15caadf5197eb4a82908ad8d729ac140e80f210f
joshua <joshua@Devons-MacBook-Air.local>
2017-04-07 00:25:15 -0600

    Basic State: Can list items Can add additional items

--- FoodTracker/FirstViewController.swift
+++ FoodTracker/FirstViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var ListingTableView: UITableView!
+    
+    @IBOutlet weak var AddButton: UIButton!
+    
+    var tableData = [["Ketchup", "4/23/2017"], ["Mustard", "6/2/2018"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //AddButton.layer.borderWidth =
+        //AddButton.layer.
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,8 +27,52 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var ListingTableView: UITableView!
-
+    @IBAction func AddButtonAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Message1", message: "Message2", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: {
+            alert -> Void in
+            
+            let fNameField = alertController.textFields![0] as UITextField
+            let lNameField = alertController.textFields![1] as UITextField
+            
+            self.tableData.append([fNameField.text!, lNameField.text!])
+            print(self.tableData)
+            self.ListingTableView.reloadData()
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
+            alert -> Void in
+        }))
+        
+        alertController.addTextField(configurationHandler: { (textField) -> Void in textField.placeholder = "Food"
+            textField.textAlignment = .center
+        })
+        
+        alertController.addTextField(configurationHandler: { (textField) -> Void in textField.placeholder = "Expiration Date"
+            textField.textAlignment = .center
+        })
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FoodDisplayCell
+        cell.NameLabel?.text = tableData[indexPath.item][0]
+        cell.ExpDateLabel?.text = tableData[indexPath.item][1]
+        return cell
+    }
 }
 
+class FoodDisplayCell: UITableViewCell{
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    @IBOutlet weak var ExpDateLabel: UILabel!
+    
+    
+}
---
+++ FoodTracker/FoodItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodItemCell.swift
+//  FoodTracker
+//
+//  Created by joshua on 4/6/17.
+//  Copyright © 2017 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class FoodItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
